991ae1d89f02e26323b9054ded0903cb68458794
Marc Mintel <marc@mintel.me>
2020-09-17 23:54:24 +0200

    add more tests

---
+++ src/lib/core/features/site/usecases/get-site/GetSite.spec.ts
@@ -0,0 +1,48 @@
+import { SiteDTO } from '../../dtos';
+import { SiteGateway } from '../../gateways';
+import { GetSite } from './GetSite';
+
+const mockPage: SiteDTO = {
+  id: '123',
+  title: 'foo',
+  logo: 'foo',
+};
+
+const mockGateway: jest.Mocked<SiteGateway> = {
+  getSite: jest.fn(),
+};
+
+describe('GetSite', () => {
+  beforeEach(() => {
+    mockGateway.getSite.mockResolvedValue(mockPage);
+  });
+
+  it('initializes without crashing', () => {
+    expect(() => new GetSite(mockGateway)).not.toThrow();
+  });
+
+  describe('execute', () => {
+    it('calls the gateway', async () => {
+      expect(mockGateway.getSite).not.toHaveBeenCalled();
+
+      const useCase = new GetSite(mockGateway);
+      await useCase.execute();
+
+      expect(mockGateway.getSite).toHaveBeenCalledTimes(1);
+    });
+
+    it('returns a page if found', async () => {
+      const useCase = new GetSite(mockGateway);
+      const page = await useCase.execute();
+
+      expect(page).toEqual(expect.objectContaining(mockPage));
+    });
+
+    it('returns an error if not found', async () => {
+      mockGateway.getSite.mockRejectedValue('Not found');
+
+      const useCase = new GetSite(mockGateway);
+      await expect(useCase.execute()).rejects.toThrow();
+    });
+  });
+});
