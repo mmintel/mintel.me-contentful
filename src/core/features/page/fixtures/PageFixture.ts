6a53b77f4aa64b4a8fead952253cf4b5fdd23bc6
Marc Mintel <marc@mintel.me>
2020-10-01 13:43:54 +0200

    satisfy linter

--- src/core/features/page/fixtures/PageFixture.ts
+++ src/core/features/page/fixtures/PageFixture.ts
@@ -1,17 +1,17 @@
-import { Page, PageProps } from "../domain";
+import { Page, PageProps } from '../domain';
 import faker from 'faker';
 
 export class PageFixture extends Page {
-    constructor(overwrites?: Partial<PageProps>) {
-        super({
-            id: faker.random.uuid(),
-            title: faker.random.word(),
-            description: faker.random.words(),
-            slug: faker.lorem.slug(),
-            components:  {
-                json: {}
-            },
-            ...overwrites,
-        })
-    }
+  constructor(overwrites?: Partial<PageProps>) {
+    super({
+      id: faker.random.uuid(),
+      title: faker.random.word(),
+      description: faker.random.words(),
+      slug: faker.lorem.slug(),
+      components: {
+        json: {},
+      },
+      ...overwrites,
+    });
+  }
 }
