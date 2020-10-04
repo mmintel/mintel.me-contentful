02f57ee8fa4851fa26564e176ca17cc4697d2404
Marc Mintel <marc@mintel.me>
2020-10-04 13:45:50 +0200

    update

--- src/components/blocks/timeline/TimelineItem.tsx
+++ src/components/blocks/timeline/TimelineItem.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 interface TimelineItemProps {
-  time: string;
+  time?: string;
   children: React.ReactNode;
 }
 
 const TimelineItem: React.FC<TimelineItemProps> = ({ time, children }) => (
   <div className="shadow-sm p-4 rounded-md bg-white">
-    <span className="font-bold">{time}</span>
+    {time && <span className="font-bold">{time}</span>}
     {children}
   </div>
 );
