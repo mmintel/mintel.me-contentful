1a7e49afe7d2d01ec49e5502f81effae70356bb1
Marc Mintel <marc@mintel.me>
2020-10-14 16:04:53 +0200

    update

--- src/context/__mocks__/AppContext.tsx
+++ src/context/__mocks__/AppContext.tsx
@@ -1,15 +1,7 @@
 import faker from 'faker';
 
-// export const useApp = jest.fn().mockReturnValue({
-//   site: {
-//     title: faker.random.words(),
-//   },
-// });
-
-export const useApp = () => {
-  return {
-    site: {
-      title: 'foo',
-    },
-  };
-};
+export const useApp = jest.fn().mockReturnValue({
+  site: {
+    title: faker.random.words(),
+  },
+});
